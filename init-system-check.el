03cde09616ad1fc19988dfe0cf0a498d5c9a222a
Yang Liu <ayliu@amazon.com>
2015-06-15 09:04:36 +0800

    add system check definition

--- init-system-check.el
+++ init-system-check.el
@@ -7,4 +7,16 @@
 (defvar *is-windows-system-p* (eq system-type 'windows-nt))
 (defvar *is-linux-system-p* (eq system-type 'gnu/linux))
 
+(defvar *is-linux-not-amazon*
+  (and *is-linux-system-p* (not *is-amazon-machine*)))
+
+(defvar *is-amazon-linux*
+  (and *is-amazon-machine* *is-linux-system-p*))
+
+(defvar *is-amazon-linux-putty*
+  (and *is-amazon-linux*  (not (window-system))))
+
+(defvar *is-amazon-linux-window*
+  (and *is-amazon-linux*  (window-system)))
+
 (provide 'init-system-check)
