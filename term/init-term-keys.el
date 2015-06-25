64af0e0b148cd7a7d8dd6626b09878429cb31b6c
Yang Liu <ayliu@amazon.com>
2015-06-25 19:03:30 +0800

    pretty format

--- term/init-term-keys.el
+++ term/init-term-keys.el
@@ -2,20 +2,20 @@
 (defun disabled-return()
   (interactive)
   (message "disabled return key"))
-  
+
 (setq my-extra-needed-key
-  '(
-    ;; ("C-a" . move-beginning-of-line)
-    ;; ("C-e" . move-end-of-line)
-    ;; ("C-@" . set-mark-command)
-    ("M-w" . kill-ring-save)
-    ;; ("C-k" . kill-line)
-    ;;("C-b" . term-send-left)
-    ;;("C-f" . term-send-right)
-    ("C-c C-k" . term-line-mode)
-    ("C-y" . term-paste)
-    ([(return)] . (lambda()(interactive) (term-send-raw-string "\C-j")))
-    ))
+      '(
+        ;; ("C-a" . move-beginning-of-line)
+        ;; ("C-e" . move-end-of-line)
+        ;; ("C-@" . set-mark-command)
+        ("M-w" . kill-ring-save)
+        ;; ("C-k" . kill-line)
+        ;;("C-b" . term-send-left)
+        ;;("C-f" . term-send-right)
+        ("C-c C-k" . term-line-mode)
+        ("C-y" . term-paste)
+        ([(return)] . (lambda()(interactive) (term-send-raw-string "\C-j")))
+        ))
 
 (dolist (pair my-extra-needed-key)
   (add-to-list 'term-bind-key-alist pair))
@@ -31,10 +31,12 @@
 (define-key term-mode-map "\C-c\C-k" 'term-toggle-between-modes)
 ;; setting keys \C-z + i(which from 1 to 8) to switch to the the ith term frame
 (dotimes (i 8)
-  (global-set-key (concat "\C-z" (number-to-string (+ i 1))) `(lambda() (interactive)
-                            (let (tn) (when (setq tn (nth ,i (if (fboundp 'multi-term-list)
-                                                                 (multi-term-list)
-                                                               multi-term-buffer-list)))
-                                        (switch-to-buffer tn))))))
+  (global-set-key
+   (concat "\C-z" (number-to-string (+ i 1)))
+   `(lambda() (interactive)
+      (let (tn) (when (setq tn (nth ,i (if (fboundp 'multi-term-list)
+                                           (multi-term-list)
+                                         multi-term-buffer-list)))
+                  (switch-to-buffer tn))))))
 
 (provide 'init-term-keys)
