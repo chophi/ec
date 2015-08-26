4d03a8ef36d43dc49cdb683b689d1b67d7d25cb2
Yang Liu <ayliu@amazon.com>
2015-08-26 10:18:03 +0800

    format c code

---
+++ init-astyle.el
@@ -0,0 +1,17 @@
+(defun program-exist(program)
+  (not (equal (shell-command-to-string (concat "which "  program)) "")))
+
+(when (program-exist "astyle")
+  (defun astyle-this-buffer(pmin pmax)
+    (interactive "r")
+    (shell-command-on-region pmin pmax
+                             "astyle --style=stroustrup"
+                             (current-buffer)
+                             t
+                             (get-buffer-create "*Astyle Errors*")
+                             t)))
+
+(require 'clang-format)
+(setq-default clang-format-style
+      "{BasedOnStyle: Google, IndentWidth: 4}")
+(provide 'init-astyle)
