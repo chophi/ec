0855d96bf0c33f4130508e65030b97e3f7604d1a
rhyflyn <revflyn@163.com>
2015-09-10 00:39:27 +0800

    replace the insert-directory-program for mac

--- init-dired.el
+++ init-dired.el
@@ -15,4 +15,8 @@
 (require-package 'dired-narrow)
 (define-key dired-mode-map (kbd "F") 'dired-narrow)
 
+;;; install MacPort, then sudo port install coreutils, and the ls program will
+;;; be available
+(if *is-mac-machine*
+    (setq insert-directory-program "/opt/local/libexec/gnubin/ls"))
 (provide 'init-dired)
