ed0e2f54153c80bc9c6e55bc3ba02b771f4d6c4e
rhyflyn <revflyn@163.com>
2015-09-12 18:18:05 +0800

    revert the width|height config

--- init-system-default-frame-alist.el
+++ init-system-default-frame-alist.el
@@ -1,7 +1,7 @@
 (setq window-system-default-frame-alist
-      '(`(x
-          (width .  ,(mf-max-display-pixel-width))
-          (height . ,(- (mf-max-display-pixel-height) 2))
+      `((x
+          (width .  ,(frame-parameter nil 'width))
+          (height . ,(- (frame-parameter nil 'height) 2))
           (font . ,(frame-parameter nil 'font))
           (top . ,(frame-parameter nil 'top))
           (left . ,(frame-parameter nil 'left))
