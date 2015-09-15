6ee9431d1eaabb1a183d36490d01503eda98d86e
Liu Yang <chophi@qq.com>
2015-09-15 11:53:03 +0800

    require dired mode before set key map

--- init-dired.el
+++ init-dired.el
@@ -13,6 +13,7 @@
 ;; (define-key dired-mode-map (kbd "F") dired-filter-map)
 
 (require-package 'dired-narrow)
+(require 'dired)
 (define-key dired-mode-map (kbd "F") 'dired-narrow)
 
 ;;; install MacPort, then sudo port install coreutils, and the ls program will
