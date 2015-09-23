5118743f0a8dfc0d6bc829ab07db779b34a8123a
Yang Liu <ayliu@amazon.com>
2015-09-23 14:19:57 +0800

    yari with [(f9)]

--- init-ruby.el
+++ init-ruby.el
@@ -1,3 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/yari.el")
 (require 'yari)
+
+(add-hook 'ruby-mode-hook (lambda () (interactive)
+                            (local-set-key [(f9)] 'yari)))
+(add-hook 'yari-mode-hook (lambda () (interactive)
+                            (local-set-key [(f9)] 'yari)))
+
 (provide 'init-ruby)
