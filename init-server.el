65e5d8e1a3f1f9ecc04da72ba2b588fb3028529c
Yang Liu <ayliu@amazon.com>
2015-10-12 15:14:13 +0800

    emacs server setting for window system

--- init-server.el
+++ init-server.el
@@ -3,7 +3,8 @@
 
 (when *is-windows-system-p*
       (setq server-auth-dir "~/.emacs.d/server"
-	    server-name "emacs-server-file"))
+	    server-name "emacs-server-file")
+      (defun server-ensure-safe-dir (dir) "Noop" t))
 
 
 (when (not (window-system))
