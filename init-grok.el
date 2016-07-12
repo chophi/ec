19d628ede368561c9ea05d017f40091e99ffcb72
Yang Liu <ayliu@amazon.com>
2016-07-12 18:15:28 +0800

    print the help message to minibuffer

--- init-grok.el
+++ init-grok.el
@@ -5,14 +5,15 @@
 (setq eopengrok-jar (concat (getenv "HOME") "/.emacs-pkg/clj-opengrok-0.3.0-standalone.jar"))
 
 (defconst my-opengrok-map
-  '((?i . eopengrok-make-index)
-    (?I . eopengrok-make-index-with-enable-projects)
+  '(;;(?i . eopengrok-make-index)
+    ;;(?I . eopengrok-make-index-with-enable-projects)
     (?d . eopengrok-find-definition)
     (?f . eopengrok-find-file)
     (?s . eopengrok-find-reference)
     (?t . eopengrok-find-text)
     (?h . eopengrok-find-history)
-    (?r . eopengrok-resume)))
+    (?r . eopengrok-resume)
+    (?p . print-eopengrok-map-help)))
 
 (my-set-global-keys "\C-c\C-g" `(,my-opengrok-map))
 
