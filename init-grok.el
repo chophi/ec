d4145c2d6ce24222338b2b626479437b0908e1ed
Yang Liu <ayliu@amazon.com>
2016-09-20 14:13:40 +0800

    Bind "RET" in eopengrok-mode
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-grok.el
+++ init-grok.el
@@ -15,6 +15,11 @@
     (?r . eopengrok-resume)
     (?p . print-eopengrok-map-help)))
 
+(define-key eopengrok-mode-map "o" 'eopengrok-jump-to-source)
+;; following is the link explains the differences between "RET" and [(return)]
+;; http://ergoemacs.org/emacs/emacs_key_notation_return_vs_RET.html
+(define-key eopengrok-mode-map (kbd "RET") 'eopengrok-jump-to-source)
+
 (my-set-global-keys "\C-c\C-g" `(,my-opengrok-map))
 
 (provide 'init-grok)
