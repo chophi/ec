5d6737190f737acd7eb15a4a7e4e36d5351cffbd
Yang Liu <ayliu@amazon.com>
2016-09-28 15:23:54 +0800

    Bind M-Q to vr/query-replace
    M-R was already binded to vr/replace
    
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-keybind.el
+++ init-keybind.el
@@ -1,7 +1,6 @@
 (global-unset-key (kbd "\C-z"))
 (global-unset-key (kbd "C-SPC"))
 
-(global-set-key (kbd "C-S-r") 'replace-string)
-(global-set-key (kbd "M-R") 'replace-regexp)
+(global-set-key (kbd "M-Q") 'vr/query-replace)
 (global-set-key (kbd "\C-xo") 'switch-window)
 (provide 'init-keybind)
