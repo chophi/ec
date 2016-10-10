a71aa444b0c88eacb45af7dde104ee999d4136ac
Yang Liu <ayliu@amazon.com>
2016-10-10 15:28:24 +0800

    Change default foreground/background colors
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init.el
+++ init.el
@@ -25,8 +25,8 @@
   (setq global-foreground-color "Black"))
 
 (when (equal use-theme "sanityinc-tommorrow")
-  (setq global-background-color "#2E3436"
-        global-foreground-color "white"
+  (setq global-background-color "#2D2D2D"
+        global-foreground-color "#CCCCCC"
         ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold]
         ansi-color-names-vector (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21")))
 
