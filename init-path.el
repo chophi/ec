c9b072fd4447ef8c779cc70d5b79e2aea654ebd9
Yang Liu <ayliu@amazon.com>
2016-10-21 15:29:44 +0800

    Utilize the init path functions
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-path.el
+++ init-path.el
@@ -1,53 +1,38 @@
-(defun path-seperator() (if *is-windows-system-p* ";" ":"))
-(defun head-to-path(new-path)
-  (setenv "PATH" (concat (expand-file-name new-path) (path-seperator) (getenv "PATH"))))
-(defun append-to-path(new-path)
-  (setenv "PATH" (concat (getenv "PATH") (path-seperator) (expand-file-name new-path))))
+(defun path-seperator()
+  (if *is-windows-system-p* ";" ":"))
 
-(let ((win-append-to-path-list
-       '("d:/ProgEnv/Ruby21-x64/bin/"
-	 "d:/ProgEnv/RubyDevkit/bin/"
-	 "~/scripts"
-	 "d:/ProgEnv/texlive/2014/bin/win32/"
-         "D:/ProgEnv/gs9.15/bin"
-         "D:/ProgEnv/R-3.1.2/bin"
-         ;; 这两个放最后
-         "d:/ProgEnv/Git/bin"
-         "d:/ProgEnv/mingw/bin"))
-      (win-exec-path-list
-       '("d:/ProgEnv/Racket/"
-         "D:/ProgEnv/gs9.15/bin"
-         "D:/ProgEnv/R-3.1.2/bin"))
-      (win-head-to-path-list
-       '(""))      
-      ;; linux path list
-      (linux-append-to-path-list
-       '(""
-	 ))
+(defun existed-directory? (path)
+  (and (file-exists-p path) (eq (car (file-attributes path)) t)))
 
-      (linux-head-to-path-list
-       '("~/.emacs.d/.emacs-bin"
-         "/usr/local/texlive/2015/bin/x86_64-linux"))
-      (linux-exec-path-list
-       '(""
-	 ))
-      (mac-append-to-path-list
-       '("~/bin"
-         "/usr/local/bin"
-         "~/bin/common-scripts"
-         "/usr/local/texlive/2015/bin/universal-darwin"
-         ))
-      (mac-exec-path-list
-       '(""))
-      (mac-head-to-path-list
-       '("")))
-  (dolist (p (if *is-windows-system-p* win-append-to-path-list (if *is-linux-system-p* linux-append-to-path-list mac-append-to-path-list)))
-    (append-to-path p))
-  (dolist (p (if *is-windows-system-p* win-exec-path-list (if *is-linux-system-p* linux-exec-path-list mac-exec-path-list)))
-    (add-to-list 'exec-path p))
-  (dolist (p (if *is-windows-system-p* win-head-to-path-list (if *is-linux-system-p*  linux-head-to-path-list mac-head-to-path-list)))
-    (head-to-path p)))
+(defun add-to-path(path &optional insert-to-head)
+  (when (existed-directory? path)
+    (if insert-to-head
+        (setenv "PATH"
+                (concat (expand-file-name path) (path-seperator) (getenv "PATH")))
+        (setenv "PATH"
+                (concat (getenv "PATH") (path-seperator) (expand-file-name path))))))
+
+(defconst *linux-extra-path-list*
+  '(("~/.emacs.d/.emacs-bin" t)
+    ("/usr/local/texlive/2015/bin/x86_64-linux"))
+  "The list of paths which will be add to path on linux if the path exists")
+
+(defconst *mac-extra-path-list*
+  '(("~/bin" t)
+    ("/usr/local/bin" t)
+    ("~/bin/common-scripts" t)
+    ("/usr/local/texlive/2016/bin/universal-darwin"))
+  "The list of paths which will be add to path on linux if the path exists")
+
+(dolist (extra-path-list
+         (if *is-mac-machine*
+             *mac-extra-path-list*
+           *linux-extra-path-list*))
+  (let ((path (car extra-path-list))
+        (insert-to-head (card extra-path-list)))
+    (add-to-path path insert-to-head)))
 
 (add-to-list 'exec-path "/usr/local/bin")
 (provide 'init-path)
 
+(getenv "PATH")"/Users/ayliu:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9:/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9:/Users/ayliu/bin:/usr/local/bin:/Users/ayliu/bin/common-scripts:/usr/local/texlive/2015/bin/universal-darwin"
