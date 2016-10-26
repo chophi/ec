840c29fd18ca9d5891292b02bb4f736b14e5cd24
Yang Liu <ayliu@amazon.com>
2016-10-26 16:28:24 +0800

    Bind M-Q to vr/replace
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-keybind.el
+++ init-keybind.el
@@ -2,5 +2,6 @@
 (global-unset-key (kbd "C-SPC"))
 
 (global-set-key (kbd "M-Q") 'vr/query-replace)
+(global-set-key (kbd "M-R") 'vr/replace)
 (global-set-key (kbd "\C-xo") 'switch-window)
 (provide 'init-keybind)
