9695e8b52c468d3f939bd90c12acc13b41a5e265
Yang Liu <ayliu@amazon.com>
2016-11-09 14:46:40 +0800

    Modify the keybind for my-env-find-file
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-uniform-env.el
+++ init-uniform-env.el
@@ -79,9 +79,8 @@
 (update-env-alist)
 
 ;; (global-set-key (kbd "C-x C-f") 'ido-find-file)
-(global-set-key (kbd "C-S-x C-S-f") 'my-env-find-file)
+(global-set-key (kbd "C-c C-f") 'my-env-find-file)
 
 ;; source emacs global variable
 (setenv "LD_LIBRARY_PATH" (concat (getenv "LD_LIBRARY_PATH") ":/usr/local/lib:/usr/local/lib32:/usr/local/lib64"))
 (provide 'init-uniform-env)
-
