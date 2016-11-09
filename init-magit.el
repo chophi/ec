7a0cd97526e3d3118d26bf583c95f53548fcde2a
Yang Liu <ayliu@amazon.com>
2016-11-09 18:15:22 +0800

    Update magit
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-magit.el
+++ init-magit.el
@@ -1,3 +1,4 @@
+(require-package 'dash)
 (require-package 'magit)
 
 (global-set-key "\C-xg" 'magit-status)
@@ -5,6 +6,6 @@
 ;; (when *is-mac-machine*
 ;;   (setq magit-git-executable "/opt/local/bin/git"))
 
-(setq magit-last-seen-setup-instructions "1.4.0")
+;; (setq magit-last-seen-setup-instructions "1.4.0")
 
 (provide 'init-magit)
