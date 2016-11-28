27f102edfb0237904a4bc32d37b4e7e5b1728efe
chophi <chophi@qq.com>
2016-11-29 01:37:53 +0800

    Enable python elpy
    Signed-off-by: chophi <chophi@qq.com>

--- init-python.el
+++ init-python.el
@@ -10,27 +10,27 @@
         python-shell-completion-string-code
         "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
 
-
-;; if it's ipython 0.10, python-shell-completion-string-code is different as below.                                    
-;; (setq python-shell-completion-string-code
-;;       "';'.join(__IP.complete('''%s'''))\n"
-;;       python-shell-completion-module-string-code "")
-
-;; (add-to-list 'load-path (concat (getenv "HOME") "\\.emacs.d\\site-lisp"))
-;; (require 'pymacs)
-
-;; (add-to-list 'load-path (concat (getenv "HOME") "\\.emacs.d\\elpa\\python-mode-6.1.3"))
-;; (setq py-install-directory (concat (getenv "HOME") "\\.emacs.d\\elpa\\python-mode-6.1.3"))
-;; (require 'python-mode)
-
-;; (setq py-shell-name "d:/ProgEnv/Python34/Scripts/ipython.exe")
-(require-package 'jedi)
-(when (not *is-mac-machine*)
-  (add-hook 'python-mode-hook 'jedi:setup))
-(setq jedi:complete-on-dot t)
 (setq-default python-indent-offset 4)
 
-(when *is-mac-machine*
-  (add-to-list 'exec-path "/opt/local/bin/"))
+;;; ---------------------------------------
+;;; https://github.com/jorgenschaefer/elpy
+;;; ---------------------------------------
+;;; pip install rope jedi flake8 importmagic autopep8 yapf
+(defun pip-installed? (packages)
+  (let ((pip-packages (shell-command-to-string "pip list"))
+        (all-installed t))
+    (dolist (pkg packages)
+      (when (not (string-match pkg pip-packages))
+        (setq all-installed nil))
+      )
+    all-installed))
+
+(if (pip-installed? '("rope" "jedi" "flake8" "importmagic" "autopep8" "yapf"))
+    (progn
+      (require-package 'elpy)
+      (elpy-enable))
+  (progn
+    (message "please install the required packages:\n sudo -H pip install rope jedi flake8 importmagic autopep8 yapf")))
+
 
 (provide 'init-python)
