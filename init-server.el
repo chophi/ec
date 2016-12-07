cad210260aa2711ecdd4f7dce84eadb7b61b641c
Yang Liu <ayliu@amazon.com>
2016-12-07 11:28:19 +0800

    Don't restart server if client server already exist
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-server.el
+++ init-server.el
@@ -1,19 +1,20 @@
 (require 'server)
 
+(when (not server-clients)
 
-(when *is-windows-system-p*
-      (setq server-auth-dir "~/.emacs.d/server"
-            server-name "emacs-server-file")
-      (make-directory server-auth-dir t)
-      (defun server-ensure-safe-dir (dir) "Noop" t))
+  (when *is-windows-system-p*
+    (setq server-auth-dir "~/.emacs.d/server"
+          server-name "emacs-server-file")
+    (make-directory server-auth-dir t)
+    (defun server-ensure-safe-dir (dir) "Noop" t))
 
+  (when (not (window-system))
+    (setq server-auth-dir "~/.emacs.d/server-nw"
+          server-name "emacs-server-file"))
+  
+  ;; (when *is-windows-system-p*
+  ;;   (defun server-ensure-safe-dir (dir) "Noop" t))
 
-(when (not (window-system))
-  (setq server-auth-dir "~/.emacs.d/server-nw"
-	server-name "emacs-server-file"))
+  (server-start))
 
-;; (when *is-windows-system-p*
-;;   (defun server-ensure-safe-dir (dir) "Noop" t))
-
-(server-start)
 (provide 'init-server)
