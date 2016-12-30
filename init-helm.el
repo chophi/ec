5f4524bb8918e201eaa4423aa6a295075d3ccc5d
Yang Liu <ayliu@amazon.com>
2016-12-30 18:09:18 +0800

    Move the helm function to helm.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-helm.el
+++ init-helm.el
@@ -1,3 +1,13 @@
 (require-package 'helm)
 (require-package 'ac-helm)
+
+;; use ido instead of helm with following use-ido-list
+(with-eval-after-load "helm"
+  (let ((use-ido-list
+         '(describe-function
+           describe-variable
+           describe-symbol)))
+    (dolist (use-ido use-ido-list)
+      (setf (cdr (assoc use-ido helm-completing-read-handlers-alist)) 'ido))))
+
 (provide 'init-helm)
--- init-python.el
+++ init-python.el
@@ -37,14 +37,5 @@
 
 (when *is-mac-machine*
   (add-to-list 'exec-path "/opt/local/bin/"))
-
-;; use ido instead of helm with following use-ido-list
-(with-eval-after-load "helm"
-  (let ((use-ido-list
-       '(describe-function
-         describe-variable
-         describe-symbol)))
-  (dolist (use-ido use-ido-list)
-    (setf (cdr (assoc use-ido helm-completing-read-handlers-alist)) 'ido))))
-
+ö
 (provide 'init-python)
