a94d0d492a4317b898d4c8cadde2115f78732c5b
chophi <chophi@qq.com>
2017-02-05 19:55:52 +0800

    Customize the org blog root path
    Signed-off-by: chophi <chophi@qq.com>

--- init-preview-org.el
+++ init-preview-org.el
@@ -1,13 +1,18 @@
 ;; config for publish site from org files
 (require 'ox-publish)
 
+(when (file-exists-p "init-private-custom.el") (require 'init-private-custom))
+(when (not (boundp '*org-blog-root*)) (setq *org-blog-root* "~/blog/"))
+(defun org-blog-path (relpath)
+  (concat *org-blog-root* relpath))
+
 (setq org-publish-project-alist
       '(
         ;; These are the main web files
         ("org-notes"
-         :base-directory "~/temp/org-export/" ;; Change this to your local dir
+         :base-directory (org-blog-path "org-export/") ;; Change this to your local dir
          :base-extension "org"
-         :publishing-directory "~/temp/www"
+         :publishing-directory (org-blog-path "www")
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4             ; Just the default for this project.
@@ -23,9 +28,9 @@
 
         ;; These are static files (images, pdf, etc)
         ("org-static"
-         :base-directory "~/temp/org-export/" ;; Change this to your local dir
+         :base-directory (org-blog-path "org-export/") ;; Change this to your local dir
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|txt\\|asc"
-         :publishing-directory "~/temp/www/"
+         :publishing-directory (org-blog-path "www/")
          :recursive t
          :publishing-function org-publish-attachment
          )
@@ -36,20 +41,20 @@
 
 (defun my-preview-org-file ()
   (interactive)
-
-  (when (not (file-directory-p "~/temp/org-export/css"))  (make-directory "~/temp/org-export/css" t))
-  (when (not (file-directory-p "~/temp/www"))  (make-directory "~/temp/www" t))
-  (when (not (file-exists-p "~/temp/org-export/css/stylesheet.css"))
-    (copy-file "~/.emacs.d/css/stylesheet.css" "~/temp/org-export/css/stylesheet.css"))
+  (when (not (file-directory-p (org-blog-path "org-export/css"))  (make-directory (org-blog-path "org-export/css")) t))
+  (when (not (file-directory-p (org-blog-path "www"))  (make-directory (org-blog-path "www")) t))
+  (when (not (file-exists-p (org-blog-path "org-export/css/stylesheet.css")))
+    (copy-file "~/.emacs.d/css/stylesheet.css" (org-blog-path "org-export/css/stylesheet.css")))
   
   (when (or (not (eq major-mode 'org-mode))
             (not (equal (file-name-extension (buffer-name)) "org"))) 
     (error "This command only works in org-mode and on org file"))  
-  (write-file (format "~/temp/org-export/%s" (buffer-name)))
+  (write-file (format (org-blog-path "org-export/%s") (buffer-name)))
   (org-publish-all)
 
   (when *is-mac-machine*
-    (shell-command (format "open ~/temp/www/%s.%s" (file-name-sans-extension (buffer-name)) "html"))))
+    (shell-command
+     (format "open %s/www/%s.%s" *org-blog-root* (file-name-sans-extension (buffer-name)) "html"))))
 
 (define-key org-mode-map [f9] 'my-preview-org-file)
 
