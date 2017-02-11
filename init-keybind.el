3098c2aba66dc8b6ac44f79f8f2f18ac91f26b27
chophi <chophi@qq.com>
2017-02-12 03:21:20 +0800

    Modify frequently used keybinding from M-Q to C-S-Q
    Signed-off-by: chophi <chophi@qq.com>

--- init-keybind.el
+++ init-keybind.el
@@ -1,7 +1,7 @@
 (global-unset-key (kbd "\C-z"))
 (global-unset-key (kbd "C-SPC"))
 
-(global-set-key (kbd "M-Q") 'vr/query-replace)
-(global-set-key (kbd "M-R") 'vr/replace)
+(global-set-key (kbd "C-S-Q") 'vr/query-replace)
+(global-set-key (kbd "C-S-R") 'vr/replace)
 (global-set-key (kbd "\C-xo") 'switch-window)
 (provide 'init-keybind)
