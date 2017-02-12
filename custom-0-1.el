77dc38485accb8e8012af30409ebfe43c9f32126
chophi <chophi@qq.com>
2017-02-12 19:47:24 +0800

    Turn on line-number-mode
    Signed-off-by: chophi <chophi@qq.com>

--- custom-0-1.el
+++ custom-0-1.el
@@ -8,7 +8,7 @@
    (quote
     ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4427a3246e50bedf4041ab92cd89cc18198220e98e3e8646dde3dc317a97dc53" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(fci-rule-color "#373b41")
- '(line-number-mode nil)
+ '(line-number-mode t)
  '(package-selected-packages
    (quote
     (helm-flycheck exec-path-from-shell flycheck yari yaml-mode whole-line-or-region wgrep w3m visual-regexp unfill undo-tree tabbar switch-window smex smart-compile skewer-mode session scratch ruby-electric ruby-additional rsense rinari regex-tool rainbow-delimiters racket-mode project-local-variables pointback paredit paper-theme pandoc-mode page-break-lines ox-pandoc org-fstree openwith noflet mwe-log-commands multiple-cursors multi-term move-text mic-paren maxframe markdown-mode+ json-mode jedi idomenu ido-ubiquitous ibuffer-vc htmlize helm-pydoc glsl-mode ggtags fill-column-indicator expand-region eopengrok elpy edit-server-htmlize dired-narrow dired+ diminish cpputils-cmake confluence color-theme-sanityinc-tomorrow cmake-mode clang-format browse-kill-ring auctex apples-mode anything ace-jump-mode ac-inf-ruby ac-helm)))
