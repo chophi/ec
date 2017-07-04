46fde7f18da65bafb8407bfad5ff270b1b9d189d
Yang Liu <ayliu@amazon.com>
2017-07-04 16:14:32 +0800

    Customize the org-bullets-bullet-list.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-org.el
+++ init-org.el
@@ -127,5 +127,8 @@
 
 (require-package 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+;; (setq org-bullets-bullet-list '("◐" "◑" "◒" "◓"))
+;; (setq org-bullets-bullet-list '("◉" "◎"))
+(setq org-bullets-bullet-list '("⚇" "⚉"))
 
 (provide 'init-org)
