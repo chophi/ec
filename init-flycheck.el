162695e2ceb54a124e4596813b74d40ae166d61c
Yang Liu <ayliu@amazon.com>
2017-07-25 16:55:39 +0800

    Use c++11 for flycheck to avoid c++11 related warnings.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- init-flycheck.el
+++ init-flycheck.el
@@ -1,4 +1,6 @@
 (require-package 'flycheck)
 (setq flycheck-global-modes '(python-mode c-mode c++-mode java-mode))
 (global-flycheck-mode)
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
 (provide 'init-flycheck)
