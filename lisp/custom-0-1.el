2b39c2d7dde0a0d1cc171c6cc59034d8d716ab27
Yang Liu <ayliu@amazon.com>
2017-08-16 14:13:52 +0800

    Copy from custom-1-1.el.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/custom-0-1.el
+++ lisp/custom-0-1.el
@@ -3,9 +3,73 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes
+   (quote
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4427a3246e50bedf4041ab92cd89cc18198220e98e3e8646dde3dc317a97dc53" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fci-rule-color "#373b41")
+ '(line-number-mode t)
  '(package-selected-packages
    (quote
-    (yari yapfify yaml-mode xml-rpc whole-line-or-region wgrep w3m visual-regexp unfill undo-tree tabbar switch-window swift-mode smex smart-compile skewer-mode session scratch ruby-electric ruby-additional rsense rinari regex-tool rainbow-delimiters racket-mode project-local-variables polymode pointback paredit paper-theme pandoc-mode page-break-lines ox-pandoc org-fstree org-bullets openwith noflet mwe-log-commands multiple-cursors multi-term move-text mic-paren maxframe markdown-mode+ json-mode jedi idomenu ido-ubiquitous ibuffer-vc htmlize helm-pydoc helm-flycheck glsl-mode ggtags flycheck-swift fill-column-indicator expand-region exec-path-from-shell eopengrok elpy edit-server-htmlize dired-narrow dired+ diminish cpputils-cmake color-theme-sanityinc-tomorrow cmake-mode clang-format browse-kill-ring auctex apples-mode anything adaptive-wrap ace-jump-mode ac-inf-ruby ac-helm))))
+    (org-bullets yapfify swift-mode flycheck-swift helm-flycheck exec-path-from-shell flycheck yari yaml-mode whole-line-or-region wgrep w3m visual-regexp unfill undo-tree tabbar switch-window smex smart-compile skewer-mode session scratch ruby-electric ruby-additional rsense rinari regex-tool rainbow-delimiters racket-mode project-local-variables pointback paredit paper-theme pandoc-mode page-break-lines ox-pandoc org-fstree openwith noflet mwe-log-commands multiple-cursors multi-term move-text mic-paren maxframe markdown-mode+ json-mode jedi idomenu ido-ubiquitous ibuffer-vc htmlize helm-pydoc glsl-mode ggtags fill-column-indicator expand-region eopengrok elpy edit-server-htmlize dired-narrow dired+ diminish cpputils-cmake confluence color-theme-sanityinc-tomorrow cmake-mode clang-format browse-kill-ring auctex apples-mode anything ace-jump-mode ac-inf-ruby ac-helm)))
+ '(safe-local-variable-values
+   (quote
+    ((bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+     (eval let*
+           ((x
+             (dir-locals-find-file default-directory))
+            (this-directory
+             (if
+                 (listp x)
+                 (car x)
+               (file-name-directory x))))
+           (unless
+               (featurep
+                (quote swift-project-settings))
+             (add-to-list
+              (quote load-path)
+              (concat this-directory "utils")
+              :append)
+             (let
+                 ((swift-project-directory this-directory))
+               (require
+                (quote swift-project-settings))))
+           (set
+            (make-local-variable
+             (quote swift-project-directory))
+            this-directory))
+     (indent-tabs-mode . true)
+     (eval progn
+           (c-set-offset
+            (quote innamespace)
+            (quote 0))
+           (c-set-offset
+            (quote inline-open)
+            (quote 0)))
+     (require-final-newline))))
+ '(session-use-package t nil (session))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
