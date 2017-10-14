d5d0269dc93a6533b8b478f12a5c2c89246a4f31
Yang Liu <ayliu@amazon.com>
2017-10-14 13:34:16 +0800

    Add swift bin for linux.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-swift.el
+++ lisp/init-swift.el
@@ -1,3 +1,10 @@
 (require-package 'swift-mode)
 (require-package 'flycheck-swift)
+(eval-after-load 'flycheck '(flycheck-swift-setup))
+
+(when *linux?*
+  (add-to-list 'exec-path "~/software/swift-dev/bin/")
+  (add-to-path "~/software/swift-dev/bin/")
+  (setq swift-mode:repl-executable "repl_swift"))
+
 (provide 'init-swift)
