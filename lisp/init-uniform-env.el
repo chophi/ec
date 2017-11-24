5c265fdfcc57c919c0fd576e26d3dd70f2f7e838
chophy <chophi@qq.com>
2017-11-24 13:56:16 +0800

    Remove the unused functions.
    Signed-off-by: chophy <chophi@qq.com>

--- lisp/init-uniform-env.el
+++ lisp/init-uniform-env.el
@@ -25,23 +25,6 @@
   '()
   "environment strings")
 
-(defun ue-read-env-and-push (env)
-  (let (value)
-    (setq value (read-string (format "%s: " env)))
-    (when (> (length value 0))
-      (shell-command (format "echo %s=%s >> %s\n" env value ue-emacs-env-file))
-      (setenv env value)
-      (add-to-list 'ue-env-alist `(,env ,value)))))
-
-(defun ue-push-env ()
-  (interactive)
-  (let (env)
-    (setq env (read-string "ENV: "))
-    (if (getenv env)
-      (error "ENV exists: [%s]=[%s]" env (getenv env))
-      (ue-read-env-and-push env)
-      (setenv env (ue-expand-env env)))))
-
 (defun ue-expand-env (env)
   ;;(message "ENV:%s" env)
   (let (value first-part second-part)
@@ -81,14 +64,13 @@
 
 (ue-update-env-alist)
 
-
 (defun ue-insert-to-env-list ()
   (interactive)
   (let ((file-path (ido-read-file-name "File PATH:"))
         (name (read-string "Name: ")))
   (with-current-buffer (find-file-noselect ue-emacs-env-file)
     (goto-char (buffer-end 1))
-    (insert (format "%s=%s" name (expand-file-name file-path)))
+    (insert (format "%s=%s\n" name (expand-file-name file-path)))
     (save-buffer)
     (ue-update-env-alist))))
 
