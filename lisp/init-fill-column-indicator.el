2d03a81ac5459562170e3648eec6c1b0e32eb1c8
Yang Liu <ayliu@amazon.com>
2018-01-02 13:23:02 +0800

    Seems not work.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-fill-column-indicator.el
+++ lisp/init-fill-column-indicator.el
@@ -36,14 +36,14 @@
     (setq-default fill-column 80)
     (global-programming-fci-mode)))
 
-;; wired character show-ed in buffers opened by org-open-at-point when
-;; I set the fci-rule-character to ?│, below is the workaround to erase them
-;; by re-turn on the fci mode if it's enabled in the buffer.
-(with-eval-after-load "org"
-  (defadvice org-open-at-point (around fci-mode-work-around)
-    ad-do-it
-    (when (and (boundp fci-mode) fci-mode)
-      (turn-on-fci-mode)))
-  (ad-activate 'org-open-at-point))
+;; ;; wired character show-ed in buffers opened by org-open-at-point when
+;; ;; I set the fci-rule-character to ?│, below is the workaround to erase them
+;; ;; by re-turn on the fci mode if it's enabled in the buffer.
+;; (with-eval-after-load "org"
+;;   (defadvice org-open-at-point (around fci-mode-work-around)
+;;     ad-do-it
+;;     (when (and (boundp fci-mode) fci-mode)
+;;       (turn-on-fci-mode)))
+;;   (ad-activate 'org-open-at-point))
 
 (provide 'init-fill-column-indicator)
