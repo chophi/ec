3f91a6b3de0a3ba133a626e137b534cf7aecd90b
Yang Liu <ayliu@amazon.com>
2018-01-09 09:48:02 +0800

    Bind C-c C-i for makefile mode also.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -83,7 +83,10 @@
       (?c . flyspell-auto-correct-word))
     "The keymap for ispell")
   (setq ctrl-c-ctrl-i-keymap (append ctrl-c-ctrl-i-keymap my-ispell-keymap))
-  (cu-set-key-bindings global-map "\C-c\C-i" ctrl-c-ctrl-i-keymap))
+  (cu-set-key-bindings global-map "\C-c\C-i" ctrl-c-ctrl-i-keymap)
+  (cu-set-key-bindings makefile-mode-map "\C-c\C-i"
+                       `(,ctrl-c-ctrl-i-keymap
+                         ((?F . makefile-insert-gmake-function)))))
 
 ;; uniform environment
 (cu-set-key-bindings global-map "\C-c\C-f"
