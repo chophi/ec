17357a58d9b9070ea0eb5650299043a3e1f4a425
chophy <chophi@qq.com>
2018-01-10 10:49:13 +0800

    Update the split rule.
    Signed-off-by: chophy <chophi@qq.com>

--- gnus/init-gnus.el
+++ gnus/init-gnus.el
@@ -18,13 +18,14 @@
                       (nnmail-expiry-wait 90)
                       (nnimap-inbox ("INBOX" "Junk"))
                       (nnimap-split-methods nnimap-split-fancy)
+                      (nnmail-split-methods nnimap-split-fancy)
                       (nnimap-split-fancy
-                       (| (from ,(concat private-foxmail-username "@foxmail\\.com") "test.myself")
+                       (| (: gnus-registry-split-fancy-with-parent)
+                          (: spam-split)
+                          (from ,(concat private-foxmail-username "@foxmail\\.com") "test.myself")
                           (to "linux-fsdevel@vger\\.kernel.org" "dev.fsdevel")
                           (to "linux-ext4@vger\\.kernel.org" "dev.ext4")
                           (to "git@vger\\.kernel\\.org" "dev.git")
-                          (: gnus-registry-split-fancy-with-parent)
-                          (: spam-split)
                           "INBOX"))))
 
 ;; To ignore the folder "^.*其他文件夹/QQ邮件订阅.*"
