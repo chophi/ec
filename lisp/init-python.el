e47ea0f5d48a33a847fae12a2bee8a7c8330b9fc
chophi <chophi@qq.com>
2018-01-22 01:32:35 +0800

    kill *Python* when exit() issued.
    Signed-off-by: chophi <chophi@qq.com>

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -13,6 +13,18 @@
   (add-hook 'python-mode-hook 'smart-shift-mode-on)
   (define-key python-mode-map (kbd "C-c C-d") 'helm-pydoc))
 
+(defun kill-buffer-when-process-finished ()
+  "Close current buffer when `shell-command' exit."
+  (let ((process (ignore-errors (get-buffer-process (current-buffer)))))
+    (when process
+      (set-process-sentinel process
+                            (lambda (proc change)
+                              (when (string-match "\\(finished\\|exited\\)" change)
+                                (kill-buffer (process-buffer proc))))))))
+
+;; Kill the *Python* buffer when exited after running "exit()"
+(add-hook 'inferior-python-mode-hook 'kill-buffer-when-process-finished)
+
 (require-package 'yapfify)
 ;;; Don't enable it by default, or everytime you save the python source code,
 ;;; it will re-format the file.
