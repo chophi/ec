f89a7f5c18a0e666e09c409dd58ee048346f3166
Yang Liu <ayliu@amazon.com>
2018-04-26 19:06:00 +0800

    Don't push if the content is not updated.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- site-python/write_wiki_content.py
+++ site-python/write_wiki_content.py
@@ -1,6 +1,8 @@
 import xmlrpclib
 import argparse
 import re
+import copy
+import os
 
 def post_process_table_content(table):
     header_row = re.findall(
@@ -21,6 +23,7 @@ def post_process_table_content(table):
         table = table.replace(link, "<a href=\"{0}\">{0}</a>".format(link))
     return table
 
+
 def pre_extract_tables(content):
     table_map = {}
     counter = 0
@@ -37,20 +40,35 @@ def pre_extract_tables(content):
     # print("content: {0}, table_map: {1}".format(content, table_map))
     return content, table_map
 
+
 def post_insert_tables(content, table_map):
     for key, value in table_map.items():
         content = content.replace(key, value)
     # print("post content: {0}".format(content))
     return content
 
+
+def remove_auto_generated(content):
+    return re.sub(r'ac:macro-id=".*"', "[AUTO_GENERATED]", content)
+
+
 def main(args):
-    client = xmlrpclib.Server(args.confluence_url, verbose = 0)
+    draft_page_id = "269528852"
+    client = xmlrpclib.Server(args.confluence_url, verbose=0)
     auth_token = client.confluence2.login(args.username, args.password)
     page = client.confluence2.getPage(auth_token, args.page_id)
+    page_draft = client.confluence2.getPage(auth_token, draft_page_id)
+    if args.action == "print-draft-wiki":
+        print("{0}".format(page_draft['content']))
     if args.action == "print-wiki-title":
-        print("title: {title}\nurl: {url}".format(
-            title=page['title'],
-            url=page['url']
+        print((
+            "Draft title: {t1}\nurl: {u1}\n"
+            "Title: {t2}\nurl: {u2}"
+        ).format(
+            t1=page['title'],
+            u1=page['url'],
+            t2=page_draft['title'],
+            u2=page_draft['url'],
         ))
     elif args.action == "print-wiki-content":
         print("{content}".format(
@@ -59,11 +77,12 @@ def main(args):
     elif args.action == "store-wiki-content":
         if page['creator'] != args.username:
             print(("The wiki {url} is not created by you\n"
-               "To avoid messing up the wiki system\n"
+                   "To avoid messing up the wiki system\n"
                    "I'd like to not update it. exit!!!").format(
                        url=page['url']
                    ))
         else:
+            original_content = page['content']
             # print("args.markup is {0}".format(args.markup))
             if args.markup == "true":
                 print("Is wiki markup and need to convert to internal storage")
@@ -77,14 +96,27 @@ def main(args):
             else:
                 print("Is html, no need to convert to internal storage")
                 page['content'] = args.content
-            print("PAGE content is {0}".format(page['content']))
-            result = client.confluence2.storePage(auth_token, page)
-            if result:
-                print("The content of {0} was updated just now!".format(
-                    page['url']
-                ))
+            # Save to draft page, and reload the content,
+            # then compare it with the original_content.
+            # if it has no change, do nothing.
+            page_draft['content'] = copy.copy(page['content'])
+            client.confluence2.storePage(auth_token, page_draft)
+            page_draft = client.confluence2.getPage(auth_token, draft_page_id)
+            with open(os.path.expanduser('~/draft'), 'w') as f:
+                f.write(page_draft['content'])
+            with open(os.path.expanduser('~/original'), 'w') as f:
+                f.write(original_content)
+            if remove_auto_generated(page_draft['content']) == remove_auto_generated(original_content):
+                print("The Page content is not changed!")
             else:
-                print("Failed to update {0} failed!".format(page['url']))
+                print("PAGE content is {0}".format(page['content']))
+                result = client.confluence2.storePage(auth_token, page)
+                if result:
+                    print("The content of {0} was updated just now!".format(
+                        page['url']
+                    ))
+                else:
+                    print("Failed to update {0} failed!".format(page['url']))
     else:
         print("error input action")
     client.confluence2.logout(auth_token)
@@ -141,7 +173,7 @@ if __name__ == "__main__" :
         '--action',
         type=str,
         help=('action to make'),
-        choices=["print-wiki-title", "print-wiki-content", "store-wiki-content"],
+        choices=["print-wiki-title", "print-wiki-content", "print-draft-wiki", "store-wiki-content"],
         required=True
     )
     args = parser.parse_args()
