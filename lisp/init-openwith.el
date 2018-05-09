916cf388cc6f908efcf215435be18c165980f932
Yang Liu <ayliu@amazon.com>
2018-05-09 22:12:50 +0800

    Add remote image showing support.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-openwith.el
+++ lisp/init-openwith.el
@@ -8,7 +8,9 @@
     '(("\\.pdf$" "evince" (file))
       ("\\.mp3$" "mplayer" (file) )
       ("\\.mov\\|\\.RM$\\|\\.RMVB$\\|\\.avi$\\|\\.AVI$\\|\\.flv$\\|\\.mp4\\|\\.mkv$\\|\\.rmvb$" "mplayer" (file) )
-      ("\\.CHM$\\|\\.chm$" "chmsee"  (file))))
+      ("\\.CHM$\\|\\.chm$" "chmsee"  (file))
+      ("\\.\\(?:jp?g\\|png\\)\\'" "remote-show-image"
+       (file))))
    ('macos
     '(("\\.pdf$" "open" (file))))))
 
