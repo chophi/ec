c6901a6029f4786518dbf3228494c45e73eefe24
Yang Liu <ayliu@amazon.com>
2018-06-13 16:23:49 +0800

    Add pathes to PATH.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-path.el
+++ lisp/init-path.el
@@ -20,27 +20,48 @@ but can also be added to head if AS-HEAD is not nil"
                 (concat p path-separator a))))))
 
 (defconst linux-extra-path-list
-  '(("~/.emacs.d/.emacs-bin" t)
-    ("~/.linux_script" t)
-    ("/usr/local/texlive/2015/bin/x86_64-linux"))
+  '(("/usr/local/texlive/2015/bin/x86_64-linux"))
   "The list of paths which will be add to path on linux if the path exists")
 
 (defconst mac-extra-path-list
-  '(("~/bin" t)
-    ("/usr/local/bin" t)
-    ("~/bin/common-scripts" t)
+  '(("/usr/local/bin" t)
     ("/usr/local/texlive/2016/bin/universal-darwin")
     ("~/Library/Android/sdk/platform-tools/")
     ("~/Library/Android/android-ndk-r10e/")
     ("~/Library/Android/sdk/tools/"))
   "The list of paths which will be add to path on linux if the path exists")
 
+(defconst bash-config-root-dir "~/git-repo/config")
+(defconst common-extra-path-list
+  (mapcar
+   (lambda (path)
+     (let ((replace-pairs
+            `(("${CONFIG_ROOT_DIR}" ,bash-config-root-dir)
+              ("${MY_HOST_SYSTEM}"
+               ,(case system-type
+                  (darwin "darwin")
+                  (gnu/linux "linux")
+                  (t "unknown"))))))
+       (dolist (p replace-pairs)
+         (setq path (replace-regexp-in-string (car p) (cadr p) path t))))
+     (list path t))
+   '("${CONFIG_ROOT_DIR}/bin/common"
+     "${CONFIG_ROOT_DIR}/bin/common/decompile-apk"
+     "${CONFIG_ROOT_DIR}/bin/${MY_HOST_SYSTEM}"
+     "${CONFIG_ROOT_DIR}/private/bin/common"
+     "${CONFIG_ROOT_DIR}/private/bin/${MY_HOST_SYSTEM}"
+     "${CONFIG_ROOT_DIR}/script/common"
+     "${CONFIG_ROOT_DIR}/script/${MY_HOST_SYSTEM}"
+     "${CONFIG_ROOT_DIR}/private/script/common"
+     "${CONFIG_ROOT_DIR}/private/script/${MY_HOST_SYSTEM}")))
 
 (dolist (extra-path-list
-         (case system-type
-           (darwin mac-extra-path-list)
-           (gnu/linux linux-extra-path-list)
-           (t '())))
+         (append
+          common-extra-path-list
+          (case system-type
+            (darwin mac-extra-path-list)
+            (gnu/linux linux-extra-path-list)
+            (t '()))))
   (let ((path (car extra-path-list))
         (as-head (cadr extra-path-list)))
     (add-to-path path as-head)))
