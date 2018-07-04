73a328df204f718127b5bc712b272d0d99d1cbf1
Yang Liu <ayliu@amazon.com>
2018-07-04 22:48:48 +0800

    dvisvgm can't find the libgs on mac.
    Signed-off-by: Yang Liu <ayliu@amazon.com>

--- lisp/init-tikz.el
+++ lisp/init-tikz.el
@@ -1,7 +1,13 @@
 (defvar tikz-latexmk-options nil)
-(defconst tikz-dvisvgm-options "-e")
+(defconst tikz-dvisvgm-options "--clipjoin --page=1-")
 (defconst tikz-output-buffer "*Generate tikz[Output]*")
 (defconst tikz-error-buffer "*Generate tikz[Error]*")
+
+(defun dvisvgm-extra-gs-args ()
+  (if (and (equal os 'mac)
+           (file-exists-p "/usr/local/lib/libgs.dylib"))
+      (format "--libgs=\"%s\"" "/usr/local/lib/libgs.dylib")
+    ""))
 
 (defun compile-tikz-to-svg ()
   (interactive)
@@ -12,9 +18,10 @@
          (svg-name (concat base-name ".svg"))
          (cmd
           (format
-           "latexmk %s %s --output-directory=%s && dvisvgm %s %s --output=%s"
+           "latexmk %s %s --output-directory=%s && dvisvgm %s %s %s --output=%s"
            (or tikz-latexmk-options " ")
            buffer-file-name tmp-dir
+           (dvisvgm-extra-gs-args)
            (or tikz-dvisvgm-options " ")
            (cu-join-path tmp-dir dvi-name)
            (cu-join-path tmp-dir svg-name)))
