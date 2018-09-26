63ff3b177a05ed1b29316b8b9abf095cd7ba0f49
chophy <chophi@qq.com>
2018-09-26 15:12:57 +0800

    Indent 4 space instead 8 on parameter lines.
    Signed-off-by: chophy <chophi@qq.com>

--- elpa/kotlin-mode-20180219.1653/kotlin-mode.el
+++ elpa/kotlin-mode-20180219.1653/kotlin-mode.el
@@ -338,7 +338,7 @@
                       (setq cur-indent (current-indentation)))
 
                      (t
-                      (setq cur-indent (+ (current-indentation) (* 2 kotlin-tab-width)))))
+                      (setq cur-indent (+ (current-indentation) kotlin-tab-width))))
                (if (< cur-indent 0)
                    (setq cur-indent 0))))
 
@@ -357,7 +357,7 @@
             ((looking-at "^[ \t]*)") ; line starts with )
              (save-excursion
                (kotlin-mode--prev-line)
-               (setq cur-indent (- (current-indentation) (* 2 kotlin-tab-width))))
+               (setq cur-indent (- (current-indentation) kotlin-tab-width)))
              (if (< cur-indent 0)
                  (setq cur-indent 0)))
 
@@ -378,7 +378,7 @@
                         (setq not-indented nil))
 
                        ((looking-at ".*([ \t]*$") ; line ends with (
-                        (setq cur-indent (+ (current-indentation) (* 2 kotlin-tab-width)))
+                        (setq cur-indent (+ (current-indentation) kotlin-tab-width))
                         (setq not-indented nil))
 
                        ((looking-at "^[ \t]*).*$") ; line starts with )
