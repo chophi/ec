74277f1c9a788aa36608be8ea8f0e5e8480f6ead
chophi <chophi@qq.com>
2018-10-21 22:32:15 +0800

    Remove the not-work-anymore google-translate package.
    Signed-off-by: chophi <chophi@qq.com>

--- lisp/init-google-translate.el
+++ lisp/init-google-translate.el
@@ -1,15 +1,2 @@
-(require-package 'google-translate)
-
-(setq-default google-translate-default-source-language "en"
-              google-translate-default-target-language "zh-CN")
-
-(setq google-translate-base-url
-      "http://translate.google.cn/translate_a/single"
-      google-translate-listen-url
-      "http://translate.google.cn/translate_tts"
-      google-translate--tkk-url
-      "http://translate.google.cn/")
-
-(setq google-translate-show-phonetic t)
 
 (provide 'init-google-translate)
