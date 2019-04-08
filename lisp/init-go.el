0ea0f2bd41c76c7031e6797a48054d34f0ee9eab
chophy <chophi@qq.com>
2019-04-08 18:40:11 +0800

    Add go flymake.
    Signed-off-by: chophy <chophi@qq.com>

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -11,14 +11,6 @@
     (?n . go-goto-function-name)
     (?r . go-goto-return-values)))
 
-(with-eval-after-load "go-mode"
-  (define-key go-mode-map
-    "\C-c\C-e"
-    '(lambda () (interactive)
-       (save-buffer)
-       (compile (format "go run %s" (buffer-name)))))
-  (cu-set-key-bindings go-mode-map "\C-c\C-f" go-mode-goto-keybindings))
-
 (add-hook 'go-common-hook
           (lambda ()
             (setq skeleton-pair t)
@@ -27,4 +19,55 @@
             (local-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
             (local-set-key (kbd "{") 'skeleton-pair-insert-maybe)
             ))
+
+(defconst possible-guru-executable
+  (format "%s/git-repo/go_tour/bin/guru" (getenv "HOME")))
+(defconst possible-go-flymake-executable
+  (format "%s/git-repo/go_tour/bin/goflymake" (getenv "HOME")))
+(defun add-go-guru-features ()
+  (require 'go-guru)
+  (defconst go-mode-go-guru-keybindings
+    '((?< . go-guru-callers)
+      (?> . go-guru-callees)
+      (?c . go-guru-peers)
+      (?d . go-guru-describe)
+      (?e . go-guru-whicherrs)
+      (?f . go-guru-freevars)
+      (?i . go-guru-implements)
+      (?j . go-guru-definition)
+      (?p . go-guru-pointsto)
+      (?r . go-guru-referrers)
+      (?s . go-guru-callstack)
+      (?x . go-guru-expand-region)))
+  (add-to-path (file-name-directory possible-guru-executable))
+  (cu-set-key-bindings go-mode-map "\C-c\C-o" go-mode-go-guru-keybindings)
+  (go-guru-hl-identifier-mode)
+  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode))
+
+(defun add-go-flymake-features ()
+  (require 'go-flymake)
+  (add-hook 'go-mode-hook (lambda () (flycheck-mode 1)))
+  ;; workaround the "go tool vet -> go vet" problem
+  (let ((govet (flycheck-checker-get 'go-vet 'command)))
+    (when (equal (cadr govet) "tool")
+      (setf (cdr govet) (cddr govet)))))
+
+(defun go-guru-exists-p ()
+  (file-executable-p possible-guru-executable))
+
+(defun go-flymake-exists-p ()
+  (file-executable-p possible-go-flymake-executable))
+
+(with-eval-after-load "go-mode"
+  (define-key go-mode-map
+    "\C-c\C-e"
+    '(lambda () (interactive)
+       (save-buffer)
+       (compile (format "go run %s" (buffer-name)))))
+  (cu-set-key-bindings go-mode-map "\C-c\C-f" go-mode-goto-keybindings)
+  (when (go-guru-exists-p)
+    (add-go-guru-features))
+  (when (go-flymake-exists-p)
+    (add-go-flymake-features)))
+
 (provide 'init-go)
