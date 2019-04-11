f48d96f52ea60ec1b3d40e5465ff62b73178b67d
chophy <chophi@qq.com>
2019-04-11 13:45:55 +0800

    Upgrade flycheck.
    Signed-off-by: chophy <chophi@qq.com>

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,6 +1,6 @@
 (require-package 'flycheck)
 (setq flycheck-global-modes '(python-mode c-mode c++-mode java-mode go-mode))
-(global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
 (add-hook 'c++-mode-hook
           (lambda () (setq flycheck-clang-language-standard "c++1z")))
 (provide 'init-flycheck)
