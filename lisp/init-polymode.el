c7662a3575ed5fd0d14b52bd3dcbe08f56b14e44
chophy <chophi@qq.com>
2019-05-06 12:31:04 +0800

    Disable poly-org mode as a workaround.
    Signed-off-by: chophy <chophi@qq.com>

--- lisp/init-polymode.el
+++ lisp/init-polymode.el
@@ -1,3 +1,8 @@
 (require-package 'polymode)
+
 (require-package 'poly-org)
+
+;; TODO: FIXME: funny char shows when poly-org-mode enabled.
+(setq auto-mode-alist  (delete '("\\.org\\'" . poly-org-mode) auto-mode-alist))
+
 (provide 'init-polymode)
