5a01cba0fff7622f88eb621021e915ada4148005
chophy <chophi@qq.com>
2019-06-26 13:38:08 +0800

    rust playground.
    Signed-off-by: chophy <chophi@qq.com>

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -20,4 +20,51 @@
                    (add-to-path dir))))))
 (add-hook 'rust-mode-hook 'cu-set-skeleton-pair-indent t)
 (define-key rust-mode-map (kbd ";") 'cu-insert-semicolon)
+
+(defun my-rust-playground-dir-name (&optional snippet-name)
+  (let ((choice
+         (ido-completing-read
+          "Choose snippet: "
+          (append (cu-list-files-recursively-general
+                   rust-playground-basedir (lambda (entry) t) 1 t)
+                  `(,(time-stamp-string "new.at-%:y-%02m-%02d-%02H%02M%02S"))))))
+    (concat
+     (cu-join-path rust-playground-basedir
+                   (if (equal (string-match "new.*" choice) 0)
+                       (substring choice 4)
+                     choice))
+     "/")))
+
+;;;###autoload
+(defun my-rust-playground ()
+  "Run playground for Rust language in a new buffer."
+  (interactive)
+  ;; get the dir name
+  (let* ((snippet-dir (my-rust-playground-dir-name))
+         (snippet-file-name (rust-playground-snippet-main-file-name snippet-dir))
+         (snippet-cargo-toml (rust-playground-toml-file-name snippet-dir)))
+    (if (file-exists-p snippet-file-name)
+        (progn
+          (find-file snippet-file-name)
+          (rust-playground-mode))
+      ;; create a buffer for Cargo.toml and switch to it
+      (make-directory snippet-dir t)
+      (set-buffer (create-file-buffer snippet-cargo-toml))
+      (set-visited-file-name snippet-cargo-toml t)
+      (rust-playground-mode)
+      (rust-playground-insert-template-head "snippet of code" snippet-dir)
+      (insert rust-playground-cargo-toml-template)
+      (save-buffer)
+      ;;now do src/main.rs
+      (make-directory (concat snippet-dir "src"))
+      (switch-to-buffer (create-file-buffer snippet-file-name))
+      (set-visited-file-name snippet-file-name t)
+      (rust-playground-insert-template-head "snippet of code" snippet-dir)
+      (insert rust-playground-main-rs-template)
+      ;; back up to a good place to edit from
+      (backward-char 27)
+      (rust-playground-mode))))
+
+(fset 'rust-playground 'my-rust-playground)
+
 (provide 'init-rust)
