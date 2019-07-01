7cbbd002b45677bfbe2545f3d45b7c1768364c8f
chophi <chophi@foxmail.com>
2019-07-01 21:13:53 +0800

    ignore the projects in .repo.
    Signed-off-by: chophi <chophi@foxmail.com>

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -4,4 +4,12 @@
 (require 'projectile)
 (projectile-mode)
 
+(defun* projectile-ignored-project-function (project-root)
+  (let ((ignored-repo-projects '("\\.repo/projects" "\\.repo/project-objects")))
+    (dolist (ignore-pattern ignored-repo-projects)
+      (when (string-match ignore-pattern project-root)
+        (return-from projectile-ignored-project-function t))
+      )
+    nil))
+
 (provide 'init-projectile)
