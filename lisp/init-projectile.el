5409704c4406b33df69101ea8e79769b1110ede7
chophi <chophi@foxmail.com>
2019-07-08 21:22:36 +0800

    Add add-repos-to-projectile.
    Signed-off-by: chophi <chophi@foxmail.com>

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -12,4 +12,25 @@
       )
     nil))
 
+(defun add-repos-to-projectile ()
+  (interactive)
+  (let* ((repo-folder "~/repo")
+         (sub-types (seq-filter (lambda (name)
+                                  (not (or (string= name ".")
+                                           (string= name ".."))))
+                                (directory-files repo-folder)))
+         (sub-repo-path nil))
+    (dolist (type sub-types)
+      (dolist (sub-repo-path
+               (seq-filter
+                (lambda (path)
+                  (and (> (length path) 0)
+                       (file-exists-p (cu-join-path repo-folder type path))))
+                (split-string
+                 (shell-command-to-string
+                  (format "cd %s && repo list --path-only"
+                          (cu-join-path repo-folder type)))
+                 "\n")))
+        (projectile-add-known-project
+         (cu-join-path repo-folder type sub-repo-path))))))
 (provide 'init-projectile)
