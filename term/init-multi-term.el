69b935fd21fa5c1f0117f7cf77ea90d42e47b3f9
chophi <chophi@foxmail.com>
2019-07-11 18:57:33 +0800

    Remove dead code.
    Signed-off-by: chophi <chophi@foxmail.com>

--- term/init-multi-term.el
+++ term/init-multi-term.el
@@ -1,13 +1,5 @@
 (require-package 'multi-term)
 
-;; the cd function provide will cause some delay when
-;; change directory to a directory which has a multi-byte name.
-;; I just make it empty to solve this problem, it's a lazy solution.
-(when (and  (not (company-computer-p)) (not (eq os 'macos)))
-    (defun cd(dir)      
-      )
-    )
-
 (cond ((and shell-file-name (file-exists-p shell-file-name))
        (setq multi-term-program shell-file-name)))
 
