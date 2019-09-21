812b320e2b38ead9d54ad89ce3030227190c309a
chophi <chophi@foxmail.com>
2019-09-22 03:25:46 +0800

    migrate more small files

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -463,4 +463,38 @@ by using nxml's indentation rules."
 ;;; wgrep setting
 (setq wgrep-change-readonly-file t)
 
+;;; rainbow-delimiters
+(defun turn-on-rainbow-delimiters-mode ()
+  (rainbow-delimiters-mode 1))
+
+(dolist (hook
+         '(emacs-lisp-mode
+           racket-mode
+           c-mode-common-hook))
+  (add-hook hook 'turn-on-rainbow-delimiters-mode))
+
+;;; Nicer naming of buffers for files with identical names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator " • ")
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
+
+;;; Spell check
+;; 1. brew install hunspell
+;; 2. Copy the en_US.aff and en_US.dic to ~/Library/Setting
+(when (equal os 'macos)
+  (let ((possible-spell-program (cu-search-brew-executable "hunspell")))
+    (when possible-spell-program
+      (setenv "DICTIONARY" "en_US")
+      (setq ispell-program-name possible-spell-program
+            ispell-dictionary "en_US")
+      (defconst my-ispell-is-enabled t "Whether ispell is enabled"))))
+
+(when (and (equal os 'linux) (executable-find "hunspell"))
+  (setenv "DICTIONARY" "en_US")
+  (setq ispell-program-name (executable-find "hunspell")
+        ispell-dictionary "en_US")
+  (defconst my-ispell-is-enabled t "Whether ispell is enabled"))
+
 (provide 'init-editing-utils)
--- lisp/init-frame-utils.el
+++ lisp/init-frame-utils.el
@@ -167,4 +167,11 @@
           `((?n . my-switch-to-next-screen-clockwise)
             (?p . my-switch-to-next-screen-anticlockwise)))))))
 
+(defun my-set-frame-name ()
+  (interactive)
+  (let ((name (read-string (concat "Frame name[" frame-default-name "]: "))))
+    (when (or (not name) (equal name ""))
+      (setq name frame-default-name))
+    (set-frame-parameter (selected-frame) 'title name)))
+
 (provide 'init-frame-utils)
